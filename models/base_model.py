fb8f232bff2ade22f7c0401831f8b7bb081d3d0c
afiorg9000 <3533@holbertonschool.com>
2022-03-01 16:21:29 +0000

    push

--- models/base_model.py
+++ models/base_model.py
@@ -3,29 +3,37 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel():
-    """"""
+    """BaseModel defines all common attributes/methods for other classes"""
+
     def __init__(self, *args, **kwargs):
+        """Public instance attributes"""
         if kwargs is not None:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
 
         for key, value in kwargs.items():
-                if key in ["created_at", "updated_at"]:
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+            """created_at && updated_at converted to ISO format string"""
+            if key in ["created_at", "updated_at"]:
+                setattr(self, key,
+                        datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
 
     def __str__(self):
-        """"""
-        return ('[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__))
+        """prints strings"""
+        return ('[{}] ({}) {}'.format
+                (self.__class__.__name__, self.id, self.__dict__))
 
+    """Public instance methods:"""
     def save(self):
-
+        """updates updated_at with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-                dictionary = self.__dict__
-                dictionary['__class__'] = self.__class__.__name__
-                dictionary['created_at'] = self.created_at.isoformat()
-                dictionary['updated_at'] = self.updated_at.isoformat()
-                return dictionary
+        """returns a dictionary containing all keys/values of __dict__"""
+        dictionary = self.__dict__
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
