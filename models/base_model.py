2098b380133c170e099d2a082be0d604c7a1731a
afiorg9000 <3533@holbertonschool.com>
2022-03-01 19:02:44 +0000

    push

--- models/base_model.py
+++ models/base_model.py
@@ -9,16 +9,18 @@ class BaseModel():
 
     def __init__(self, *args, **kwargs):
         """Public instance attributes"""
-        if kwargs is not None:
+        if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
-        for key, value in kwargs.items():
-            """created_at && updated_at converted to ISO format string"""
-            if key in ["created_at", "updated_at"]:
-                setattr(self, key,
-                        datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+        else:
+            for key, value in kwargs.items():
+                """created_at && updated_at converted to ISO format string"""
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def __str__(self):
         """prints strings"""
@@ -28,11 +30,11 @@ class BaseModel():
     """Public instance methods:"""
     def save(self):
         """updates updated_at with the current datetime"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
-        dictionary = self.__dict__
+        dictionary = self.__dict__.copy()
         dictionary['__class__'] = self.__class__.__name__
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
