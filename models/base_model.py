3c441266b05fb7bcf0ad9cf07edde0139ae54c5d
afiorg9000 <3533@holbertonschool.com>
2022-03-01 16:07:24 +0000

    base

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,31 @@
+#!/usr/bin/python3
 
+import uuid
+from datetime import datetime
+
+class BaseModel():
+    """"""
+    def __init__(self, *args, **kwargs):
+        if kwargs is not None:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
+        for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+
+    def __str__(self):
+        """"""
+        return ('[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+                dictionary = self.__dict__
+                dictionary['__class__'] = self.__class__.__name__
+                dictionary['created_at'] = self.created_at.isoformat()
+                dictionary['updated_at'] = self.updated_at.isoformat()
+                return dictionary
