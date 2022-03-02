33f649e4d33ef64d2c3d8e696713f6f2e9b73a22
gabriel337 <g.david.lopezruiz@gmail.com>
2022-03-02 14:00:51 +0000

    add file

---
+++ models/engine/file_storage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+"""
+
+class FileStorage():
+    """ """
+
+    def __init__(self, file_path, objects):
+        """ """
+        self.file_path = file_path
+        self.__objects = objects
+
+    @property
+    def file_path(self):
+        """getter returns fiel_path"""
+        return self.__file_path
+
+    @file_path.setter
+    def file_path(self
+
+
+    def all(self):
+        """ """
+
+
+    def new(self, obj):
+        """ """
+
+    def save(self):
+    """ """
+
+    def reload(self):
+        """ """
