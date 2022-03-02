c34a4f8aa0633ce05cb8e0762fe8ce17764c697e
afiorg9000 <3533@holbertonschool.com>
2022-03-02 15:09:20 +0000

    push

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,33 +1,41 @@
 #!/usr/bin/python3
-"""
-"""
+
+from models.base_model import BaseModel
+import json
+
 
 class FileStorage():
-    """ """
+    """Class that serializes instances and deserializes JSON file"""
 
-    def __init__(self, file_path, objects):
-        """ """
-        self.file_path = file_path
-        self.__objects = objects
-
-    @property
-    def file_path(self):
-        """getter returns fiel_path"""
-        return self.__file_path
-
-    @file_path.setter
-    def file_path(self
+    def __init__(self, __file_path, objects):
+        """Initializes this class"""
+        self.__file_path = file.json
+        self.__objects = {}
 
 
     def all(self):
-        """ """
-
+        """Returns the dictionary __objects"""
+        return self.__objects
 
     def new(self, obj):
-        """ """
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = obj.__class__.name + '.' + obj.id
+        __objects.update(key, obj)
 
     def save(self):
-    """ """
+    """Serializes __objects to the JSON file with __file_path"""
+        for key, value in __objects.items():
+            __objects[key] = value.__objects
+
+        with open(__file_path, 'wb') as file:
+            json.dump(__objects, file)
 
     def reload(self):
-        """ """
+        """deserializes the JSON file to __objects"""
+        try:
+            with open(__file_path, 'rb') as file:
+                __objects = json.load(file)
+
+        except FileNotFoundError:
+            pass
+
