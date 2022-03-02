75e1a0eed47efdc622cb786e938fe957b88a759f
afiorg9000 <3533@holbertonschool.com>
2022-03-02 17:52:55 +0000

    push

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""File Storage"""
 
 from models.base_model import BaseModel
 import json
@@ -7,35 +8,37 @@ import json
 class FileStorage():
     """Class that serializes instances and deserializes JSON file"""
 
-    def __init__(self, __file_path, objects):
-        """Initializes this class"""
-        self.__file_path = file.json
-        self.__objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
 
     def all(self):
         """Returns the dictionary __objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
-        key = obj.__class__.name + '.' + obj.id
-        __objects.update(key, obj)
+        FileStorage.__objects[obj.__class__.__name__ + '.' + obj.id] = obj
 
     def save(self):
-    """Serializes __objects to the JSON file with __file_path"""
-        for key, value in __objects.items():
-            __objects[key] = value.__objects
+        """Serializes __objects to the JSON file with __file_path"""
 
-        with open(__file_path, 'wb') as file:
-            json.dump(__objects, file)
+        json_dict = {}
+
+        for key, value in FileStorage.__objects.items():
+            json_dict[key] = value.to_dict()
+
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump(json_dict, file)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
-            with open(__file_path, 'rb') as file:
-                __objects = json.load(file)
+            json_dict = {}
+            with open(FileStorage.__file_path, 'r') as file:
+                json_dict = json.load(file)
+                for key, value in json_dict.items():
+                    FileStorage.__objects[key] =
 
         except FileNotFoundError:
             pass
-
