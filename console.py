e1a00ed724cf038468974a8b75ee17117b98f4ce
afiorg9000 <3533@holbertonschool.com>
2022-03-03 17:14:53 +0000

    push

--- console.py
+++ console.py
@@ -3,10 +3,12 @@
 """
 
 import cmd
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """Simple command processor example."""
     prompt = '(hbnb) '
+    class_name = {"BaseModel": BaseModel}
 
     def do_quit(self, line):
         """Quit command to exit the program\n """
@@ -19,6 +21,45 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """empty line if no command is given"""
         pass
+
+    def do_create(self, arg):
+        """creates a new instance of BaseModel, saves it and prints the id"""
+        if len(arg) <= 0:
+            print("** class name missing **")
+        elif arg[0] not in self.class_name:
+            print("** class doesn't exist **")
+        else:
+            models = eval(class_name)().save()
+            print(model.id)
+
+    def do_show(self, arg):
+        """"""
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in self.class_name:
+            print("** class doesn't exist **")
+
+
+    def do_destroy(self, arg):
+        """"""
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in self.class_name:
+            print("** class doesn't exist **")
+
+
+    def do_all(self, arg):
+        """"""
+
+    def do_update(self, arg):
+        """"""
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in self.class_name:
+            print("** class doesn't exist **")
+
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
