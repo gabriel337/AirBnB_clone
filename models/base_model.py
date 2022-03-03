ba440d7a37a576561756e4033679aa14f33c6dd6
gabriel337 <g.david.lopezruiz@gmail.com>
2022-03-03 14:18:38 +0000

    add storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,25 +2,26 @@
 
 import uuid
 from datetime import datetime
-
+import models
 
 class BaseModel():
     """BaseModel defines all common attributes/methods for other classes"""
 
     def __init__(self, *args, **kwargs):
         """Public instance attributes"""
-        if len(kwargs) == 0:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-
-        else:
+        if kwargs:
             for key, value in kwargs.items():
                 """created_at && updated_at converted to ISO format string"""
                 if key in ['created_at', 'updated_at']:
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 if key != '__class__':
                     setattr(self, key, value)
+        else:
+
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+            models.storage.new(self)
 
     def __str__(self):
         """prints strings"""
@@ -31,6 +32,7 @@ class BaseModel():
     def save(self):
         """updates updated_at with the current datetime"""
         self.updated_at = datetime.utcnow()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
