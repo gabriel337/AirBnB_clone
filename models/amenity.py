fa6f7a4c17300a6f7e0193f60279be11cbd49256
gabriel337 <g.david.lopezruiz@gmail.com>
2022-03-04 15:11:16 +0000

    add classes

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module Amenity"""
+
+import json
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """"Containes class attr name """
+
+    name = ""
